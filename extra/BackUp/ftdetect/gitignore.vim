e5e3bb64c205bca99c83279d6bbbd7cb4532bb8b
RayZ0rr <rayz0rr@protonmail.com>
2023-09-02 22:34:26 +0200

    fix: remove old files

--- extra/BackUp/ftdetect/gitignore.vim
+++
@@ -1,6 +0,0 @@
-" File:         gitignore.vim
-" Description:  .gitignore plugin for Vim
-" Author:       gisphm <phmfk@hotmail.com>
-" URL:          https://github.com/gisphm/vim-gitignore
-
-au BufNewFile,BufRead .gitignore,.ignore,.fdignore set filetype=gitignore
--- extra/BackUp/lua/plugins/general/asyncRunTasks.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_set_keymap( 'n','<leader><F7>', [[<cmd>AsyncRun clang++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wsign-conversion "$(VIM_FILEPATH)" -o "$(VIM_FILEDIR)/$(VIM_FILENOEXT)"<cr>]],{noremap=true} )
-vim.api.nvim_set_keymap( 'n','<leader><F9>', [[:AsyncRun -raw -cwd=$(VIM_FILEDIR) "$(VIM_FILEDIR)/$(VIM_FILENOEXT)" <cr>]],{noremap=true} )
-vim.api.nvim_set_keymap( 'n','<leader><F10>', [[:call asyncrun#quickfix_toggle(6)<cr>]],{noremap=true} )
-
-vim.g.asyncrun_rootmarks = {'.svn', '.git', '.root', '_darcs', 'build.xml'}
-vim.g.asyncrun_open = 6
-vim.g.asyncrun_bell = 1
-
