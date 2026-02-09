6c44230b90f1e57acc9570e42923f077a00c3c1e
RayZ0rr <rayz0rr@protonmail.com>
2026-02-09 19:58:08 +0100

    perf: try to improve matchup performance

--- nvim/lua/config/plugins/general/vim-matchup.lua
+++ nvim/lua/config/plugins/general/vim-matchup.lua
@@ -3,8 +3,11 @@ local nnmap = require('config/options/utils').nnmap
 vim.g.matchup_matchparen_offscreen = { method = 'popup'}
 vim.matchup_surround_enabled = 1
 vim.matchup_delim_stopline = 1500
-vim.g.matchup_matchparen_deferred = 1
-vim.g.matchup_matchparen_deferred_show_delay = 50
+vim.g.matchup_delim_noskips = 1
+vim.g.matchup_matchparen_stopline = 100
+vim.g.matchup_treesitter_stopline = 100
+vim.g.matchup_matchparen_deferred = 100
+vim.g.matchup_matchparen_deferred_show_delay = 500
 vim.g.matchup_matchparen_deferred_hide_delay = 700
 vim.g.matchup_matchparen_hi_surround_always = 1
 
