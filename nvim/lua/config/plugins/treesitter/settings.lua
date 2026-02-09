d70dc06a26e1be10204a8cf9628055f9d580e91e
RayZ0rr <rayz0rr@protonmail.com>
2026-02-09 20:00:54 +0100

    chore: cleanup treesitter keymaps for more convenience
    mainly for swap parameters keymap

--- nvim/lua/config/plugins/treesitter/settings.lua
+++ nvim/lua/config/plugins/treesitter/settings.lua
@@ -22,52 +22,55 @@ vim.api.nvim_create_autocmd('FileType', {
         -- syntax highlighting, provided by Neovim
         vim.treesitter.start()
         -- folds, provided by Neovim
-        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.wo.foldmethod = 'expr'
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
         -- indentation, provided by nvim-treesitter
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end,
 })
 
-require("nvim-treesitter-textobjects").setup {
-  select = {
-    -- Automatically jump forward to textobj, similar to targets.vim
-    lookahead = true,
-    selection_modes = {
-      ['@parameter.outer'] = 'v', -- charwise
-      ['@function.outer'] = 'V', -- linewise
-      ['@class.outer'] = '<c-v>', -- blockwise
-    },
-    include_surrounding_whitespace = false,
-  },
-}
+local ok_text_obj, text_obj = pcall(require, 'nvim-treesitter-textobjects')
+if ok_text_obj then
+    text_obj.setup {
+        select = {
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = 'V', -- blockwise
+            },
+            include_surrounding_whitespace = false,
+        },
+    }
 
--- keymaps
-vim.keymap.set({ "x", "o" }, "af", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
-end)
-vim.keymap.set({ "x", "o" }, "if", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
-end)
-vim.keymap.set({ "x", "o" }, "ac", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
-end)
-vim.keymap.set({ "x", "o" }, "ic", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
-end)
-vim.keymap.set({ "x", "o" }, "as", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
-end)
-vim.keymap.set("n", "<leader>tsa", function()
-  require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
-end)
-vim.keymap.set("n", "<leader>tsA", function()
-  require("nvim-treesitter-textobjects.swap").swap_previous "@parameter.outer"
-end)
+    -- keymaps
+    vim.keymap.set({ "x", "o" }, "af", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "if", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ac", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "ic", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "as", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
+    end)
+    vim.keymap.set("n", "<localleader><left>", function()
+        require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
+    end)
+    vim.keymap.set("n", "<localleader><right>", function()
+        require("nvim-treesitter-textobjects.swap").swap_previous "@parameter.outer"
+    end)
 
 
-local ok_ts_context, _ = pcall(require, 'ts_context_commentstring')
-if ok_ts_context then
-    vim.g.skip_ts_context_commentstring_module = true
-    require('ts_context_commentstring').setup {}
+    local ok_ts_context, _ = pcall(require, 'ts_context_commentstring')
+    if ok_ts_context then
+        vim.g.skip_ts_context_commentstring_module = true
+        require('ts_context_commentstring').setup {}
+    end
 end
