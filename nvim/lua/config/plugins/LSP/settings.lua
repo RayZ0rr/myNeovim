4325edc6e300e3ca9b5dbc9b8c83a603c6cce65a
RayZ0rr <rayz0rr@protonmail.com>
2026-05-03 17:36:46 +0200

    fix: LSP warning on fn to set log level

--- nvim/lua/config/plugins/LSP/settings.lua
+++ nvim/lua/config/plugins/LSP/settings.lua
@@ -1,7 +1,7 @@
 --##########################################################################################################
 -- LSP customization---------------------------------------------------------------------------
 --###################################################################################################################
-vim.lsp.set_log_level 'warn'
+vim.lsp.log.set_level 'warn'
 
 vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
 vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
